ecee8960e6e24dcb104a2df643da8f50c46c70c9
Abhiroop Santra <abhiroop.santra@gmail.com>
2026-07-21 01:08:24 +0400

    chore: commit ef new migration

---
+++ Infrastructure/ServerManagement.Infrastructure/Migrations/ApplicationDb/20260720205305_ServerSchemaUpdate.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ServerManagement.Infrastructure.Migrations.ApplicationDb
+{
+    /// <inheritdoc />
+    public partial class ServerSchemaUpdate : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "OperatingSystem",
+                schema: "ServerMgmt",
+                table: "Servers",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "OperatingSystem",
+                schema: "ServerMgmt",
+                table: "Servers");
+        }
+    }
+}
