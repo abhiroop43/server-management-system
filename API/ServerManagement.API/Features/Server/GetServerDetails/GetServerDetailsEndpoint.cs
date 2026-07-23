c3e9d2620e57b867cd632ef679350435dbe9750f
Abhiroop Santra <abhiroop43@users.noreply.github.com>
2026-07-24 01:31:18 +0400

    chore: created stubs for get server fetails

---
+++ API/ServerManagement.API/Features/Server/GetServerDetails/GetServerDetailsEndpoint.cs
@@ -0,0 +1,11 @@
+﻿namespace ServerManagement.API.Features.Server.GetServerDetails;
+
+public record GetServerDetailsResponse();
+
+public class GetServerDetailsEndpoint : ICarterModule
+{
+    public void AddRoutes(IEndpointRouteBuilder app)
+    {
+        throw new NotImplementedException();
+    }
+}
---
+++ API/ServerManagement.API/Features/Server/GetServerDetails/GetServerDetailsHandler.cs
@@ -0,0 +1,17 @@
+﻿namespace ServerManagement.API.Features.Server.GetServerDetails;
+
+public record GetServerDetailsQuery(Guid ServerId) : IQuery<GetServerDetailsResult>;
+
+public record GetServerDetailsResult();
+
+public class GetServerDetailsQueryHandler(ApplicationDbContext dbContext)
+    : IQueryHandler<GetServerDetailsQuery, GetServerDetailsResult>
+{
+    public Task<GetServerDetailsResult> Handle(
+        GetServerDetailsQuery request,
+        CancellationToken cancellationToken
+    )
+    {
+        throw new NotImplementedException();
+    }
+}
